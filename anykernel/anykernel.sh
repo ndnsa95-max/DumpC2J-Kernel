6a14786ddc0dd5fb3393c435fd78ddbe2c81b543
DumpC2J <ndnsa95@gmail.com>
2026-06-23 03:02:07 +0700

    fix: update anykernel.sh with DumpC2J branding

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kono Ha Kernel
+kernel.string=DumpC2J Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -34,14 +34,24 @@ no_magisk_check=1
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
 
-ui_print "Flashing Kono Ha Kernel..."
+ui_print " "
+ui_print " DumpC2J Kernel"
+ui_print " Running : $kernel_version"
+ui_print " "
+ui_print " Flashing boomkernel, please wait..."
+ui_print " "
 
-# boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
+    split_boot
+    flash_boot
 else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+    dump_boot
+    write_boot
 fi
+
+ui_print " "
+ui_print " New Kernel Loaded."
+ui_print " your phone is now placebo-powered, enjoy the illusion."
+ui_print " "
+
 ## end boot install
